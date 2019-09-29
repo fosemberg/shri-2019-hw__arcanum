880fa5eaf3945af4228b1c7bfddff44ecd4cfd90
fosemberg <fosemberg@gmaill.com>
2019-09-29 15:29:01 +0300

    delete mvc

--- front/common.blocks/mvc/emitter.js
+++
@@ -1,8 +0,0 @@
-const emitter = new EventTarget();
-
-listener = () => {};
-const eventName = '';
-
-emitter.addEventListener(eventName, listener);
-emitter.removeEventListener(eventName, listener);
-emitter.dispatchEvent(eventName['some']);
